50b999e61752574f370437e47bca9090025065d5
brrrbot <qpsbrrr842@gmail.com>
2025-09-02 16:36:53 +0800

    run build

--- bot/src/commands/utils/stop.ts
+++ bot/src/commands/utils/stop.ts
@@ -6,7 +6,7 @@ export async function stop(interaction: ButtonInteraction) {
     if (!queue) return void interaction.followUp({ content: "There is no queue in this server" });
     queue.delete();
     const embed = new EmbedBuilder()
-        .setColor(0x1db954)
+        .setColor("#FFFFFF")
         .setDescription("Music player is stopped")
         .setFooter({ text: "why you bully me?🥺" });
     await interaction.followUp({ embeds: [embed] });
